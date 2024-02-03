ed859ad01fd3fad01cae41fb007b5ba87df7f696
Sviatoslav <downadow@ro.ru>
2024-02-03 22:09:10 +0300

    upd

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# автор            downadow (Sviatoslav)
+
+if [[ "$2" == "" || "$1" == "--help" ]] then
+	echo "Использование:  bash build.sh [--help] ПУТЬ_К_MAKEXM2C-TOOLS ТИП"
+	echo
+	echo "ТИП может быть одним из следующих:"
+	ls etc
+	exit
+fi
+
+cd os-builder
+javac --release 8 downadow/xmtwolime_builder/main/Builder.java
+cd ../software
+java -cp ../os-builder/ downadow.xmtwolime_builder.main.Builder -p ../etc/$2/ ../kernel.S $(ls) > ../image
+cd ..
+java -cp "$1" downadow.makexm2c_tools.main.Assembler image image
