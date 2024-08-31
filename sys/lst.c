acd2ba98dae413ddb3b08c0e4ffc2c443a7b1db6
Sviatoslav <downadow@ro.ru>
2024-08-31 19:25:37 +0300

    upd. prog

--- sys/lst.c
+++ sys/lst.c
@@ -1,7 +1,9 @@
 /* вывести список установленных программ и библиотек */
 
 int main(void) {
-    for(char *s = (char *) __extern_label("system_apps"); *s != '\0'; *s++) {
+    char *s;
+    
+    for(s = (char *) __extern_label("system_apps"); *s; s++) {
         if(*s == ':') {
             printf("%c%c%c ", s[-3], s[-2], s[-1]);
         }
