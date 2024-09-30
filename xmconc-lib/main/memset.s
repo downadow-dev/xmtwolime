31db65b23922a71e2a49123089810326a3d2a53a
Sviatoslav <downadow@ro.ru>
2024-09-30 12:00:52 +0300

    новые функции для повышения производительности

---
+++ xmconc-lib/main/memset.s
@@ -0,0 +1,22 @@
+
+__0_memset:
+    ; размер
+    dec %R_FA_9%
+    ild %R_FA_9%, %R_FA_2%
+    ; символ
+    dec %R_FA_9%
+    ild %R_FA_9%, %R_FA_1%
+    ; начальный адрес
+    dec %R_FA_9%
+    ild %R_FA_9%, %R_FA_0%
+    
+    mov2 %R_FA_3%, <LIB_memset>
+    LIB_memset:
+        isv %R_FA_1%, %R_FA_0%
+        inc %R_FA_0%
+        dec %R_FA_2%
+        
+        if %R_FA_2% > %R_ZERO%, %R_FA_3%
+    
+    jmp %R_FA_8%
+
