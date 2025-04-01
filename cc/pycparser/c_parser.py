b106bcb261e06c5770129e887274784d8657df85
Sviatoslav <downadow998@gmail.com>
2025-04-01 13:34:42 +0300

    update pycparser

--- cc/pycparser/c_parser.py
+++ cc/pycparser/c_parser.py
@@ -1580,6 +1580,18 @@ class CParser(PLYParser):
         """ labeled_statement : DEFAULT COLON pragmacomp_or_statement """
         p[0] = c_ast.Default([p[3]], self._token_coord(p, 1))
 
+    def p_labeled_statement_4(self, p):
+        """ labeled_statement : ID COLON """
+        p[0] = c_ast.Label(p[1], c_ast.EmptyStatement(self._token_coord(p, 1)), self._token_coord(p, 1))
+
+    def p_labeled_statement_5(self, p):
+        """ labeled_statement : CASE constant_expression COLON """
+        p[0] = c_ast.Case(p[2], [c_ast.EmptyStatement(self._token_coord(p, 2))], self._token_coord(p, 1))
+
+    def p_labeled_statement_6(self, p):
+        """ labeled_statement : DEFAULT COLON """
+        p[0] = c_ast.Default([c_ast.EmptyStatement(self._token_coord(p, 1))], self._token_coord(p, 1))
+
     def p_selection_statement_1(self, p):
         """ selection_statement : IF LPAREN expression RPAREN pragmacomp_or_statement """
         p[0] = c_ast.If(p[3], p[5], None, self._token_coord(p, 1))
@@ -1879,15 +1891,12 @@ class CParser(PLYParser):
         """ constant    : FLOAT_CONST
                         | HEX_FLOAT_CONST
         """
-        if 'x' in p[1].lower():
+        if p[1][-1] in ('f', 'F'):
             t = 'float'
+        elif p[1][-1] in ('l', 'L'):
+            t = 'long double'
         else:
-            if p[1][-1] in ('f', 'F'):
-                t = 'float'
-            elif p[1][-1] in ('l', 'L'):
-                t = 'long double'
-            else:
-                t = 'double'
+            t = 'double'
 
         p[0] = c_ast.Constant(
             t, p[1], self._token_coord(p, 1))
