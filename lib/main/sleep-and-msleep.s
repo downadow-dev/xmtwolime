dc33dbd94086e199abb26dbc7205161e42a95f2e
Sviatoslav <downadow998@gmail.com>
2025-04-18 04:19:46 +0300

    upd. lib

--- lib/main/sleep-and-msleep.s
+++ lib/main/sleep-and-msleep.s
@@ -8,8 +8,14 @@ __0_sleep:
 	time %R_FA_1%
 	add %R_FA_0% %R_FA_1%, %R_FA_0%
 	mov %R_FA_2%, <LIB_sleep>
+	
+    mov %R_RETURN_CODE%, 0
 	LIB_sleep:
-	    nop
+        ; Ctrl+C check
+        mov %R_FA_1%, 3
+        mov %R_FA_0%, <LIB_exit>
+        if %R_KEY% == %R_FA_1%, %R_FA_0%
+        
 	    time %R_FA_1%
 	    if %R_FA_1% < %R_FA_0%, %R_FA_2%
 	
@@ -21,8 +27,14 @@ __0_msleep:
 	time %R_FA_1%
 	add %R_FA_0% %R_FA_1%, %R_FA_0%
 	mov %R_FA_2%, <LIB_msleep>
+	
+    mov %R_RETURN_CODE%, 0
 	LIB_msleep:
-	    nop
+        ; Ctrl+C check
+        mov %R_FA_1%, 3
+        mov %R_FA_0%, <LIB_exit>
+        if %R_KEY% == %R_FA_1%, %R_FA_0%
+        
 	    time %R_FA_1%
 	    if %R_FA_1% < %R_FA_0%, %R_FA_2%
 	
